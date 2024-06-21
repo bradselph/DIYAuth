4d3d10f2d2be27ef2be8d022fa19a3c52cc14462
bradselph <Bselph512@gmail.com>
2024-06-21 01:52:45 -0400

    Update main.go

--- main.go
+++ main.go
@@ -15,18 +15,15 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-// Account struct for storing account information
 type Account struct {
 	Name   string `json:"name"`
 	Secret string `json:"secret"`
 }
 
-// Storage struct for storing multiple accounts
 type Storage struct {
 	Accounts []Account `json:"accounts"`
 }
 
-// Function to load storage from a JSON file
 func loadStorage(filename string) (*Storage, error) {
 	data, err := os.ReadFile(filename)
 	if err != nil {
@@ -44,7 +41,6 @@ func loadStorage(filename string) (*Storage, error) {
 	return &storage, nil
 }
 
-// Function to save storage to a JSON file
 func saveStorage(filename string, storage *Storage) error {
 	data, err := json.MarshalIndent(storage, "", "  ")
 	if err != nil {
@@ -53,30 +49,27 @@ func saveStorage(filename string, storage *Storage) error {
 	return os.WriteFile(filename, data, 0644)
 }
 
-// Method to add an account to storage
 func (s *Storage) addAccount(name, secret string) {
 	s.Accounts = append(s.Accounts, Account{Name: name, Secret: secret})
 }
 
 func generateTOTP(secret string) (string, error) {
-	// Decode the base32 secret if necessary
-	secretBytes, err := base32.StdEncoding.DecodeString(secret)
-	if err != nil {
-		// If decoding fails, assume the secret is already in the correct format
-		secretBytes = []byte(secret)
-	}
-
-	passcode, err := totp.GenerateCode(string(secretBytes), time.Now())
+	passcode, err := totp.GenerateCode(secret, time.Now())
 	if err != nil {
 		return "", err
 	}
 	return passcode, nil
 }
 
-func decodeMigrationData(data string) ([]Account, error) {
+func decodeMigrationData(data string, debug bool) ([]Account, error) {
 	decoded, err := base64.StdEncoding.DecodeString(data)
 	if err != nil {
 		return nil, fmt.Errorf("failed to decode base64 data: %v", err)
+	}
+
+	if debug {
+		fmt.Printf("Decoded data length: %d bytes\n", len(decoded))
+		fmt.Printf("Decoded data (hex): %x\n", decoded)
 	}
 
 	var payload MigrationPayload
@@ -85,63 +78,23 @@ func decodeMigrationData(data string) ([]Account, error) {
 		return nil, fmt.Errorf("failed to unmarshal protobuf: %v", err)
 	}
 
+	if debug {
+		fmt.Printf("Number of OTP parameters: %d\n", len(payload.OtpParameters))
+	}
+
 	var accounts []Account
 	for i, otp := range payload.OtpParameters {
+		if debug {
+			fmt.Printf("OTP parameter %d raw data: %x\n", i, otp.RawData)
+		}
+
 		secret := base32.StdEncoding.EncodeToString(otp.RawData)
 		accounts = append(accounts, Account{
-			Name:   fmt.Sprintf("Imported Account %d", i+1),
+			Name:   fmt.Sprintf("Account %d", i),
 			Secret: secret,
 		})
 	}
 	return accounts, nil
-}
-
-func parseOtpParameter(data []byte) (name, issuer, secret string, err error) {
-	for len(data) > 0 {
-		tag := data[0]
-		fieldNum := tag >> 3
-		wireType := tag & 0x7
-		data = data[1:]
-
-		switch fieldNum {
-		case 1: // name
-			if wireType != 2 {
-				return "", "", "", fmt.Errorf("invalid wire type for name")
-			}
-			length := int(data[0])
-			name = string(data[1 : length+1])
-			data = data[length+1:]
-		case 2: // issuer
-			if wireType != 2 {
-				return "", "", "", fmt.Errorf("invalid wire type for issuer")
-			}
-			length := int(data[0])
-			issuer = string(data[1 : length+1])
-			data = data[length+1:]
-		case 3: // secret
-			if wireType != 2 {
-				return "", "", "", fmt.Errorf("invalid wire type for secret")
-			}
-			length := int(data[0])
-			secret = base32.StdEncoding.EncodeToString(data[1 : length+1])
-			data = data[length+1:]
-		default:
-			// Skip unknown fields
-			switch wireType {
-			case 0: // varint
-				for data[0]&0x80 != 0 {
-					data = data[1:]
-				}
-				data = data[1:]
-			case 2: // length-delimited
-				length := int(data[0])
-				data = data[length+1:]
-			default:
-				return "", "", "", fmt.Errorf("unsupported wire type: %d", wireType)
-			}
-		}
-	}
-	return
 }
 
 func tryManualDecode(data []byte) {
@@ -158,7 +111,7 @@ func tryManualDecode(data []byte) {
 	fmt.Printf("Field number: %d\n", fieldNumber)
 	fmt.Printf("Wire type: %d\n", wireType)
 
-	if wireType == 2 { // Length-delimited field
+	if wireType == 2 {
 		length := int(data[1])
 		fmt.Printf("Length: %d\n", length)
 		if len(data) >= length+2 {
@@ -176,11 +129,14 @@ func main() {
 	}
 
 	reader := bufio.NewReader(os.Stdin)
+	debugMode := false
+
 	for {
 		fmt.Println("\n1. Add Account")
 		fmt.Println("2. Show TOTPs")
 		fmt.Println("3. Migrate from Google Authenticator")
-		fmt.Println("4. Exit")
+		fmt.Println("4. Toggle Debug Mode")
+		fmt.Println("5. Exit")
 		fmt.Print("Choose an option: ")
 
 		option, err := reader.ReadString('\n')
@@ -236,7 +192,7 @@ func main() {
 				migrationData = strings.TrimPrefix(migrationData, "otpauth-migration://offline?data=")
 			}
 
-			accounts, err := decodeMigrationData(migrationData)
+			accounts, err := decodeMigrationData(migrationData, debugMode)
 			if err != nil {
 				log.Printf("Error decoding migration data: %v", err)
 				continue
@@ -244,18 +200,11 @@ func main() {
 
 			fmt.Printf("Decoded %d accounts\n", len(accounts))
 			for i, account := range accounts {
-				fmt.Printf("Account %d\n", i+1)
-				fmt.Print("Enter a name for this account: ")
-				name, err := reader.ReadString('\n')
-				if err != nil {
-					log.Printf("Error reading input: %v", err)
-					continue
-				}
-				name = strings.TrimSpace(name)
-				if name == "" {
-					name = account.Name
-				}
-				storage.addAccount(name, account.Secret)
+				fmt.Printf("Account %d: Name=%s, Secret length=%d\n", i, account.Name, len(account.Secret))
+			}
+
+			for _, account := range accounts {
+				storage.addAccount(account.Name, account.Secret)
 			}
 
 			if err := saveStorage(storageFile, storage); err != nil {
@@ -266,6 +215,14 @@ func main() {
 			fmt.Println("Accounts migrated successfully")
 
 		case "4":
+			debugMode = !debugMode
+			if debugMode {
+				fmt.Println("Debug mode enabled")
+			} else {
+				fmt.Println("Debug mode disabled")
+			}
+
+		case "5":
 			fmt.Println("Exiting...")
 			return
 
